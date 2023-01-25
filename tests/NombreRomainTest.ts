b9d80f6906912916b25da529e60803eb477823ed
Marin Tostivint <marin.tostivint@epsi.fr>
2023-01-25 10:22:16 +0100

    test 11

--- tests/NombreRomainTest.ts
+++ tests/NombreRomainTest.ts
@@ -151,4 +151,19 @@ describe("Test Chiffres Romain", function () {
 
         assert.equal(chiffreRomain, "X");
     });
+
+    it("ETANT donne le chiffre 11 QUAND on convertit le chiffre en nombre romain ALORS le nombre est XI", function () {
+       
+        // Etant donne le chiffre 11
+        let chiffreArabe : number = 11;
+
+
+        // Quand on covertit le chiffre en nombre romain
+
+        let chiffreRomain : string = NombreRomain.convertirChiffreRomain(chiffreArabe);
+
+        // ALORS le nombre romain est XI
+
+        assert.equal(chiffreRomain, "XI");
+    });
 });
