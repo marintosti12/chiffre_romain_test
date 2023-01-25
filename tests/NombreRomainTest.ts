332fb4784014a8021193c807c0b020c0269b10bc
Marin Tostivint <marin.tostivint@epsi.fr>
2023-01-25 10:12:56 +0100

    test 3

--- tests/NombreRomainTest.ts
+++ tests/NombreRomainTest.ts
@@ -19,7 +19,7 @@ describe("Test Chiffres Romain", function () {
 
     it("ETANT donne le chiffre 2 QUAND on convertit le chiffre en nombre romain ALORS le nombre est II", function () {
        
-        // Etant donne le chiffre 1
+        // Etant donne le chiffre 2
         let chiffreArabe : number = 2;
 
 
@@ -27,8 +27,23 @@ describe("Test Chiffres Romain", function () {
 
         let chiffreRomain : string = NombreRomain.convertirChiffreRomain(chiffreArabe);
 
-        // ALORS le nombre romain est I
+        // ALORS le nombre romain est II
 
         assert.equal(chiffreRomain, "II");
+    });
+
+    it("ETANT donne le chiffre 3 QUAND on convertit le chiffre en nombre romain ALORS le nombre est III", function () {
+       
+        // Etant donne le chiffre 3
+        let chiffreArabe : number = 3;
+
+
+        // Quand on covertit le chiffre en nombre romain
+
+        let chiffreRomain : string = NombreRomain.convertirChiffreRomain(chiffreArabe);
+
+        // ALORS le nombre romain est III
+
+        assert.equal(chiffreRomain, "III");
     });
 });
