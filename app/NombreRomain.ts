e4247c20314d78d55f527efdaf6af860b5710e02
Marin Tostivint <marin.tostivint@epsi.fr>
2023-01-25 14:26:33 +0100

    add test13

--- app/NombreRomain.ts
+++ app/NombreRomain.ts
@@ -2,7 +2,7 @@ export class NombreRomain
 {
     static convertirChiffreRomain(chiffreRomain: number) : string
     {
-        let romains : string[] = ["I", 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'];
+        let romains : string[] = ["I", 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII'];
 
         return romains[chiffreRomain - 1];
     }
