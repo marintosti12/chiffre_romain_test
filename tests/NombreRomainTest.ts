a5c7c7a5f94f0bb3f3cf9f4c19e2e4f410add309
Marin Tostivint <marin.tostivint@epsi.fr>
2023-01-25 10:20:48 +0100

    test 8

--- tests/NombreRomainTest.ts
+++ tests/NombreRomainTest.ts
@@ -106,4 +106,19 @@ describe("Test Chiffres Romain", function () {
 
         assert.equal(chiffreRomain, "VII");
     });
+
+    it("ETANT donne le chiffre 8 QUAND on convertit le chiffre en nombre romain ALORS le nombre est VIII", function () {
+       
+        // Etant donne le chiffre 8
+        let chiffreArabe : number = 8;
+
+
+        // Quand on covertit le chiffre en nombre romain
+
+        let chiffreRomain : string = NombreRomain.convertirChiffreRomain(chiffreArabe);
+
+        // ALORS le nombre romain est VIII
+
+        assert.equal(chiffreRomain, "VIII");
+    });
 });
