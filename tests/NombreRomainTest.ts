64f26742cea5bc6b81e9a4b2601a2765aead9a81
Marin Tostivint <marin.tostivint@epsi.fr>
2023-02-14 13:59:37 +0100

    testing 10

--- tests/NombreRomainTest.ts
+++ tests/NombreRomainTest.ts
@@ -210,6 +210,6 @@ describe("Test Chiffres Romain", function () {
 
         // ALORS le nombre romain est XIII
 
-        assert.equal(chiffreRomain, "XIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+        assert.equal(chiffreRomain, "XIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
     });
 });
